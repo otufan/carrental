553990e6ce41bbc4056900dc3d1133139b1bdc29
Omer Tufan <omertufanca@gmail.com>
2022-02-16 20:27:06 +0100

    16.02.22

--- src/main/java/com/lecture/carrental/repository/UserRepository.java
+++ src/main/java/com/lecture/carrental/repository/UserRepository.java
@@ -3,9 +3,17 @@ package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // @Query("SELECT u FROM User u WHERE u.email=?1") --> Default olarak olusturuluyor
+    Optional<User> findByEmail(String email);
 
 }
