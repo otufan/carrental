1f10ba18f703d9bec90927ff5e2b52510813e94a
Omer Tufan <omertufanca@gmail.com>
2022-02-16 22:20:15 +0100

    16.02.22

--- src/main/java/com/lecture/carrental/security/jwt/JwtUtils.java
+++ src/main/java/com/lecture/carrental/security/jwt/JwtUtils.java
@@ -1,14 +1,18 @@
 package com.lecture.carrental.security.jwt;
 import com.lecture.carrental.security.service.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
-
+@Component
 public class JwtUtils {
 
+    private static final Logger logger= LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${backendapi.app.jwtSecret}")
     private String jwtSecret;
@@ -38,5 +42,27 @@ public class JwtUtils {
     }
 
 
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        }
+        catch (SecurityException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        }
+        catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        }
+        catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        }
+        catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
 
 }
