498fb7c6934e7892fd260089924f3e455aef9984
Omer Tufan <omertufanca@gmail.com>
2022-03-01 22:23:15 +0100

    01.03.22

--- src/main/java/com/lecture/carrental/service/ReservationService.java
+++ src/main/java/com/lecture/carrental/service/ReservationService.java
@@ -1,9 +1,20 @@
 package com.lecture.carrental.service;
 
+import com.lecture.carrental.domain.Car;
+import com.lecture.carrental.domain.Reservation;
+import com.lecture.carrental.domain.User;
+import com.lecture.carrental.domain.enumeration.ReservationStatus;
+import com.lecture.carrental.exception.BadRequestException;
+import com.lecture.carrental.exception.ResourceNotFoundException;
+import com.lecture.carrental.repository.CarRepository;
 import com.lecture.carrental.repository.ReservationRepository;
+import com.lecture.carrental.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -12,5 +23,55 @@ public class ReservationService {
 
         // @Autowired --> @AllArgsConstructor kullanmak istemedigimizde kullanabiliriz... ama bu durumda final olmamali
         private final ReservationRepository reservationRepository;
+
+        private final UserRepository userRepository;
+
+        private final CarRepository carRepository;
+
+        private final static String USER_NOT_FOUND_MSG="user with id %d not found";
+
+        private final static String CAR_NOT_FOUND_MSG = "car with id %d not found";
+
+
+
+
+        public void addReservation(Reservation reservation, Long userId, Car carId) throws BadRequestException {
+
+                boolean checkStatus=carAvailability(carId.getId(), reservation.getPickUpTime(), reservation.getDropOffTime());
+
+                User user=userRepository.findById(userId).orElseThrow(()->
+                        new ResourceNotFoundException(String.format(USER_NOT_FOUND_MSG, userId)));
+
+                if (!checkStatus)
+                        reservation.setStatus(ReservationStatus.CREATED);
+                else
+                        throw new BadRequestException("Car is already reserved! Please choose another");
+
+                reservation.setCarId(carId);
+                reservation.setUserId(user);
+
+                Double totalPrice=totalPrice(reservation.getPickUpTime(), reservation.getDropOffTime(), carId.getId());
+
+                reservation.setTotalPrice(totalPrice);
+
+                reservationRepository.save(reservation);
+
+        }
+
+        public boolean carAvailability(Long carId, LocalDateTime pickUpTime, LocalDateTime dropOffTime){
+
+                List<Reservation> checkStatus=reservationRepository.checkStatus(carId, pickUpTime, dropOffTime, ReservationStatus.DONE, ReservationStatus.CANCELED);
+
+
+                return checkStatus.size()>0;
+
+        }
+
+        public Double totalPrice(LocalDateTime pickUpTime, LocalDateTime dropOffTime, Long carId) {
+                Car car = carRepository.findById(carId).orElseThrow(() ->
+                        new ResourceNotFoundException(String.format(CAR_NOT_FOUND_MSG, carId)));
+                Long hours = (new Reservation()).getTotalHours(pickUpTime, dropOffTime);
+                return car.getPricePerHour() * hours;
+        }
 
 }
